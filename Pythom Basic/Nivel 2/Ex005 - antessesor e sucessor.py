ac1d304787e2a8d4ec0773a3c347b70bedbed7c8
Rexgoku <josearthurcamilo.30@gmail.com>
2024-07-10 09:50:30 -0300

    Atualização

---
+++ Pythom Basic/Nivel 2/Ex005 - antessesor e sucessor.py
@@ -0,0 +1,4 @@
+n1 = int(input("Digite um numero:"))
+ant = n1 - 1
+sucs = n1 + 1
+print("O antessesor de {} é: {} \nO sucessor de {} é: {}".format(n1, ant, n1, sucs))
---
+++ Pythom Basic/Nivel 2/Ex006 - Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,5 @@
+n = int(input("Digite um número:"))
+do = 2 * n
+Tri = 3 * n
+Ra = n ** (1/2)
+print("O dobro do número {} é: {} \nO Triplo do número {} é: {} \nA Raiz Quadrada de {} é: {}".format(n, do, n, Tri, n, Ra))
